442dd1066a51b0df20ce749226780c86233ea6bd
Maksim Kozhukh <wkiskas@Maksims-MacBook-Air.local>
2023-07-02 04:56:23 +0300

    fix deploy

--- src/index.ts
+++ src/index.ts
@@ -41,6 +41,6 @@ bot.api.setWebhook(process.env.WEBHOOK_URL);
 const app = express();
 app.use(express.json());
 app.use(webhookCallback(bot, "express"));
-app.listen(3000, () => {
+app.listen(80, () => {
   console.log(`Bot webhook listening on ${process.env.WEBHOOK_URL}`);
 });
