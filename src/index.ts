c2e784214c72e87a7360379ac6369f5ef4b6b570
Maksim Kozhukh <wkiskas@Maksims-MacBook-Air.local>
2023-07-03 18:15:59 +0300

    Moved to webhooks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 (await import("dotenv")).config();
+import express from "express";
+import { webhookCallback } from "grammy";
 import bot from "./bot.js";
 
-bot.start();
+const secretPath = process.env.BOT_TOKEN;
+
+const app = express();
+app.use(express.json());
+app.use(`/${secretPath}`, webhookCallback(bot, "express"));
+
+app.listen(Number(process.env.PORT), async () => {
+  await bot.api.setWebhook(`https://${process.env.DOMAIN}/${secretPath}`);
+});
